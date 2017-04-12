66a55a8262c3fedd37efbd535541ab19f71a62be
Ahmed Mahmoud <ahmedmahmoud241986@gmail.com>
2017-04-12 19:05:35 -0400

    Update Needleman.py

--- Needleman.py
+++ Needleman.py
@@ -1,31 +1,7 @@
-# author Ahmed Mahmoud
-#
-# Copyright (C) 2014 by Ahmed Mahmoud
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
- 
+
 """
  
 This is an example to implement Needleman-Wunsch sequence algorithm using python
-
- 
--Ahmed Mahmoud, 12/8/2014
 
 """
  
@@ -36,34 +12,18 @@ matchScore, mismatchScore, gapScore = 4, -10,-4 # high mismatchscore will genera
 seq1 = raw_input("enter reference")
 seq2 = raw_input("enter seq")
  
-### cellMap is a dictionary that maps integer pairs to the co-routines
+
 cellMap = {}
  
-### For tracking the global best alignment cell
+
 globalBestCellScore = [None, -100000]
  
  
  
 def getAnAlignCell(x, y, seq1, seq2):
-    """
-    This function returns a co-routine the represents an alignment cell at position
-    x and y.  The alignment strings are passed explicilty for simplicity.
-    """
+  
  
     def alnCell():
- 
-        """
-        This is the co-routine for an alignment cell. A alignment cell co-routine is
-        excuted in roughly two stage. The first stage it collects the alignment score
-        from the cells at (x-1,y-1), (x-1,y), and (x, y-1) and calculate the best 
-        alignment score. Depending the alignment path through the alignment cell, a new
-        alignment score is generated and passed to the cells at (x+1, y+1), 
-        (x+1,y), and (x, y+1). If any of those cell has not be generated, it will 
-        generate the co-routine and regisiter them with the cellMap dictionary. After 
-        this it waits for the backtracking caculation.  If a cell is in the best alignment
-        path, it will pass the best alignment pair to next cell in the best alignment
-        path.
-        """
  
         global globalBestCellScore
         global cellMap
@@ -73,10 +33,6 @@ def getAnAlignCell(x, y, seq1, seq2):
  
         cellData = []
  
-        #if the cells are at the very top of the matrix (nothing above them) or left(nothing to their left
-        # they will wait for another cell to generate an alignment score. all other cells willget scores from (x-1,y), (x,y-1), and (x-1, y-1)
-        # (top column-1 row down to row 1, 1st row - 1 cell with the first column and then to the actual cell).
-        # before they can do any calculation.
         if x == 0 or y == 0:
             cellId, s = yield
             cellData.append( (cellId, s) )
@@ -147,17 +103,17 @@ def getAnAlignCell(x, y, seq1, seq2):
     return alnCell
  
  
-# initialize the cell at (0,0)
+
 cellMap[ (0,0) ] = getAnAlignCell( 0, 0, seq1, seq2 )()
 # prime it
 cellMap[(0,0)].next()
-# start the whole execution by sending in the initial score to cell at (0,0)
+
 cellMap[(0,0)].send( ( (-1, -1), 0 ) )
  
-# get the best global cell
+
 bestCell = globalBestCellScore[0]
  
-# continue to excute the best cell co-routine to get the alignment path
+
 bestPath = cellMap[bestCell].next()
 bestPath.reverse()
  
